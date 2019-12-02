57bbfc760735e4df5171c988d7c997de350450b6
Martin Schiegl <43884428+schmar00@users.noreply.github.com>
2019-12-02 14:58:01 +0100

    Add files via upload

---
+++ js/ws.js
@@ -0,0 +1,21 @@
+﻿// webservices
+"use strict";
+var ws_micka = {
+    endpoint: 'https://resource.geolba.ac.at/PoolParty/sparql/geoera_keyword',
+
+    json: function (query, thenFunc) {
+        return fetch(this.endpoint + '?query=' + encodeURIComponent(query) + '&format=application/json')
+            .then(res => res.json())
+            .then(thenFunc);
+    }
+};
+
+var ws_keywords = {
+    endpoint: ' https://resource.geolba.ac.at/PoolParty/sparql/geoera_keyword',
+
+    json: function (query, thenFunc) {
+        return fetch(this.endpoint + '?query=' + encodeURIComponent(query) + '&format=application/json')
+            .then(res => res.json())
+            .then(thenFunc);
+    }
+};
